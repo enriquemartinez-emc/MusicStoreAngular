1ee3402842d60a7976bd8b98ec19516c73de158c
Enrique Martinez <emartinezc@gmail.com>
2021-02-17 17:00:08 -0600

    initial router configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './router';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { declarations } from './core';
